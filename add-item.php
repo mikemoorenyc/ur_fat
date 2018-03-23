59825567f4aa2c884aa2ddf88145d16fd5255995
Mike Moore <tigerhollowpoint@gmail.com>
2018-03-23 10:42:55 -0400

    Update add-item.php

--- add-item.php
+++ add-item.php
@@ -1,1 +1,11 @@
+<?php
+require_once 'header.php';
 
+//MAKE SURE THE POST IS LEGIT
+if(!$_SESSION['logged_in'] || !$_SESSION['current_user'] || !$_POST || $_SESSION['add_noonce'] !== $_POST['add_noonce']) {
+  die('Bad request');
+}
+
+
+
+?>
