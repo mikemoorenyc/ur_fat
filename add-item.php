c5f95a3ce6e310610b261368c6b3614883f1b8fa
Mike Moore <tigerhollowpoint@gmail.com>
2018-03-23 20:59:33 -0400

    asfgsdfg

--- add-item.php
+++ add-item.php
@@ -2,10 +2,36 @@
 require_once 'header.php';
 
 //MAKE SURE THE POST IS LEGIT
-if(!$_SESSION['logged_in'] || !$_SESSION['current_user'] || !$_POST || $_SESSION['add_noonce'] !== $_POST['add_noonce']) {
+
+if(!$_SESSION['logged_in'] || !$_SESSION['current_user'] || !$_POST || $_SESSION[$_POST['noonce_key']] !== $_POST[$_POST['noonce_key']]) {
   die('Bad request');
 }
 
+//READY TO GO
+var_dump($_POST);
+$date = time();
 
+if($_POST['item_date']) {
+
+}
+$title = mysql_real_escape_string($_POST['item_title']);
+$amount = mysql_real_escape_string($_POST['item_amount']);
+$desc = mysql_real_escape_string($_POST['item_description']);
+
+$author = $_SESSION['current_user'];
+$insert = "INSERT INTO posts (post_title, post_description, post_amount, author, post_date)
+  VALUES ('$title','$desc','$amount','$author','$date')";
+
+die();
+
+
+$insert_item = mysqli_query($db_conn, $insert);
+
+if ($insert_item) {
+    echo 'added';
+} else {
+  echo 'bad';
+}
+$_SESSION[$_POST['noonce_key']] = '';
 
 ?>
--- index.php
+++ index.php
@@ -11,14 +11,25 @@ if(!$_SESSION['logged_in']) {
 }
 ?>
 <form id="add-item" method="POST" action="add-item.php">
-<label for="item_title">Food</label>
-<input type="text" required id="item_title" name="item_title" />
-<label for="item_amount">Amount</label>
-<INPUT type="text" id="item_amount" name="item_amount" />
-<label for="item_description">Notes</label>
-<textarea id="item_description" name="item_description"></textarea>
-<label for="item_date">When</label>
-<input type="datetime-local" />
-<button type="submit">Add Item</button>
+  <?php $_SESSION['add_item_noonce'] = genToken(); ?>
+  <input type="hidden" id="add_item_noonce" name="add_item_noonce" value="<?= $_SESSION['add_item_noonce'];?>" />
+  <input type="hidden" id="add_noonce_key" name="noonce_key" value="add_item_noonce" />
+  <label for="item_title">Food</label>
+  <input type="text" required id="item_title" name="item_title" />
+  <label for="item_amount">Amount</label>
+  <INPUT type="text" id="item_amount" name="item_amount" />
+  <label for="item_description">Notes</label>
+  <textarea id="item_description" name="item_description"></textarea>
+  <label for="item_date">When</label>
+  <input type="datetime-local" name="item_date" name="item_date"/>
+  <button type="submit">Add Item</button>
 
 </form>
+<?php
+$now = new DateTime();
+$ny_time  = new DateTimeZone('America/New_York');
+$now->setTimezone($ny_time);
+
+echo strtotime($now->format('Y-m-d 00:00:00 O')).'<br/><br/>';
+echo strtotime($now->format('Y-m-d 23:59:59 O')).'<br/><br/>';
+ ?>
