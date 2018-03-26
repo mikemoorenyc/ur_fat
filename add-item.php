35376ee00febd0b34194e589ec8f55939c478fcd
Mike Moore <tigerhollowpoint@gmail.com>
2018-03-26 10:54:53 -0400

    Update add-item.php

--- add-item.php
+++ add-item.php
@@ -1,11 +1,7 @@
 <?php
 require_once 'header.php';
 
-//MAKE SURE THE POST IS LEGIT
-
-if(!$_SESSION['logged_in'] || !$_SESSION['current_user'] || !$_POST || $_SESSION[$_POST['noonce_key']] !== $_POST[$_POST['noonce_key']]) {
-  die('Bad request');
-}
+require_once 'request_check.php';
 
 //READY TO GO
 
