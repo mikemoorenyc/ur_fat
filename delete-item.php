6812be4288ae4f8d6e8eb6a456b0db87ecc45134
Mike Moore <tigerhollowpoint@gmail.com>
2018-03-26 11:06:07 -0400

    Update delete-item.php

--- delete-item.php
+++ delete-item.php
@@ -1,1 +1,23 @@
+<?php
+require_once 'header.php'
+require_once 'request_check.php';
 
+//WE ARE READY TO DELETE
+
+$delete =  "DELETE FROM posts WHERE id=$_POST['delete_id']";
+$delete_item = mysqli_query($db_conn, $delete);
+
+
+if ($delete_item) {
+    echo 'deleted';
+} else {
+  echo 'bad';
+}
+
+
+
+$_SESSION[$_POST['noonce_key']] = '';
+header("Location: ".$_SERVER['HTTP_REFERER']);
+die();
+
+?>
