9ec9e23358ab7464e43b789c21ddd291fd61c5e1
Mike Moore <tigerhollowpoint@gmail.com>
2018-03-27 12:48:25 -0400

    Update login-user.php

--- api/login-user.php
+++ api/login-user.php
@@ -1,6 +1,14 @@
 <?php
 require_once '../header.php';
 
+if(check_user()) {
+  echo json_encode(array(
+    "logged_in" => true,
+    "id" => $_SESSION['current_user']
+  ));
+  die();
+}
+
 if(check_remember_me()) {
   $_SESSION['logged_in'] = true;
   $_SESSION['current_user'] = check_remember_me()['id'];
@@ -20,8 +28,18 @@ function badLoginResponse() {
 if(!$_POST['email'] || $_POST['password']) {
   badLoginResponse();
 }
-if(!verify_login()) {
+$login_check = verify_login();
+if(!$login_check) {
   badLoginResponse();
+} else {
+  $_SESSION['logged_in'] = true;
+  $_SESSION['current_user'] = $login_check;
+  create_remember_me();
+  echo json_encode(array(
+    "logged_in" => true,
+    "id" => $_SESSION['current_user']
+  ));
+  die();
 }
 
 
