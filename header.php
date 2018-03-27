b30999dc61fc51f29c25240cf441d55f632ee518
Mike Moore <tigerhollowpoint@gmail.com>
2018-03-27 10:43:39 -0400

    Update header.php

--- header.php
+++ header.php
@@ -7,6 +7,14 @@ include 'func_user_check.php';
 include 'func_request_check.php';
 include 'func_check_remember_me.php';
 
+$dir = new DirectoryIterator('/global_functions');
+foreach ($dir as $i) {
+    if($i->getExtension() !== 'php' || !$i->isFile()) {
+     continue;
+    }
+    include_once $i->getPathname();
+}
+
 require 'db_connect.php';
 if($need_to_install) {
   die('<a href="install.php">Install URFAT</a>');
