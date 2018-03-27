6c9781a318aa58d1664f5106f9efd82aed282a60
Mike Moore <tigerhollowpoint@gmail.com>
2018-03-27 10:36:53 -0400

    Update validate_date.php

--- global_functions/validate_date.php
+++ global_functions/validate_date.php
@@ -1,6 +1,6 @@
 <?php
 
-function validateDate($date, $format = 'Y-m-dTh:iA')
+function validate_date($date, $format = 'Y-m-dTh:iA')
 {
     $d = DateTime::createFromFormat($format, $date);
     return $d && $d->format($format) == $date;
