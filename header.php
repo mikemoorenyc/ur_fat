0794cb91007f238919ec90db819b8e9e38d17a1c
Mike Moore <tigerhollowpoint@gmail.com>
2018-03-27 10:44:42 -0400

    Update header.php

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <?php
 date_default_timezone_set('UTC');
 session_start();
-
+/*
 include 'func_genToken.php';
 include 'func_user_check.php';
 include 'func_request_check.php';
 include 'func_check_remember_me.php';
-
+*/
 $dir = new DirectoryIterator('/global_functions');
 foreach ($dir as $i) {
     if($i->getExtension() !== 'php' || !$i->isFile()) {
