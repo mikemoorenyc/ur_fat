c6b42196c9914433035ffdb32ff2c612c928a9fd
Mike Moore <tigerhollowpoint@gmail.com>
2018-03-29 10:41:01 -0400

    Update get_posts.php

--- global_functions/get_posts.php
+++ global_functions/get_posts.php
@@ -1,9 +1,22 @@
 <?php
 
-function get_posts($user_id,$from,$to) {
-  if(!$user_id) {
-    return false;
-  }
+function get_posts($from,$to,$limit,$page,$user_id) {
+ $where_queries = [];
+ if($user_id) {
+  $where_queries[] = 'post_author ='.intval($user_id);
+ }
+ if($from) {
+  $where_queries[] = "post_date >=".intval($from); 
+ }
+ if($to) {
+  $where_queries[] = "post_date <=".intval($to);
+ }
+ if($limit) {
+   $limit_query = "LIMIT ".intval($limit);
+ }
+ if($page) {
+   $offset_query = "OFFSET ".((intval($page) - 1) * intval($limit));
+ }
 
 
 
