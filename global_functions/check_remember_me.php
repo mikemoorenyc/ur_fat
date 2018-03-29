593780f1800e5e273692cd0ddd0590bb011adced
Mike Moore <tigerhollowpoint@gmail.com>
2018-03-29 18:01:20 -0400

    Update check_remember_me.php

--- global_functions/check_remember_me.php
+++ global_functions/check_remember_me.php
@@ -1,6 +1,12 @@
 <?php
 function verify_remember_me() {
-
+ if(!$_COOKIE['remember_me']) {
+  return false;
+ }
+ $credentials = json_decode($_COOKIE['remember_me'],true);
+ if(!$credentials['selector'] || !$credentials['validator']) {
+  return false;
+ }
 }
 
 
