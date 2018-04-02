e5524af98ee5f38f937dccd6ced9be2230e4e3a5
Mike Moore <tigerhollowpoint@gmail.com>
2018-04-02 17:09:40 -0400

    Update verify_login.php

--- global_functions/verify_login.php
+++ global_functions/verify_login.php
@@ -8,7 +8,7 @@ function verify_login($email=null,$password=null) {
     $get_user =  "SELECT * FROM users WHERE `email` = '".mysql_real_escape_string($email)."' LIMIT 1";
 
   $user = $db_conn->query($get_user);
-  if($user->num_rows < 1) {
+  if($user->num_rows < 1 || !$user) {
     return false;
   }
   $user = $user->fetch_assoc();
@@ -19,7 +19,7 @@ $pass_pass = password_verify(base64_encode(hash('sha256', $password, true)),$use
   if(!$pass_pass) {
     return false;
   }
-  $current_user = get_user($user['id']);
+
 
   return login_user($user['id']);
 
