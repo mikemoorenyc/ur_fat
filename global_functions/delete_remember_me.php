301f6baa675e7cb0fc9742a19239dc2503684dbd
Mike Moore <tigerhollowpoint@gmail.com>
2018-04-02 14:23:05 -0400

    Update delete_remember_me.php

--- global_functions/delete_remember_me.php
+++ global_functions/delete_remember_me.php
@@ -2,14 +2,16 @@
 function delete_remember_me() {
   global $db_conn;
    $credentials = json_decode($_COOKIE['ur_fat_remember_me'],true);
-   var_dump($credentials);
+
    $sql =  "DELETE FROM tokens WHERE selector=".intval($credentials['selector']);
    $deleted_token =  mysqli_query($db_conn, $sql);
    if($deleted_token) {
-     return 'deleted';
+     return true;
      unset($_COOKIE["ur_fat_remember_me"]);
+   } else {
+     return false;
    }
-   return $deleted_token;
+   
 }
 
  ?>
