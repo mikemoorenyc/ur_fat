7a4a7a9b464b9008cf28f99612c0a3d81ac53869
Mike Moore <tigerhollowpoint@gmail.com>
2018-04-03 13:34:05 -0400

    Update form-process-add-item.php

--- form-process-add-item.php
+++ form-process-add-item.php
@@ -14,5 +14,5 @@ $date = parse_time($_POST['post_date']) ?: time();
 
 $item = add_item($_POST['post_title'],$_POST['post_description'],$_POST['post_amount'],$date,$author);
 
-unset($_SESSION['add_item_noonce'])
+unset($_SESSION['add_item_noonce']);
 header("Location: index.php",TRUE,303);
