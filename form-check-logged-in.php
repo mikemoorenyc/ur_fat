4e46590ddc1df0ede36d87f6073c6fac27fd5605
Mike Moore <tigerhollowpoint@gmail.com>
2018-04-03 13:24:18 -0400

    Update form-check-logged-in.php

--- form-check-logged-in.php
+++ form-check-logged-in.php
@@ -1,1 +1,18 @@
+<?php
+require_once 'header.php';
 
+$logged_in = is_user_logged_in();
+$response = array();
+if($logged_in) {
+ $response['logged_in'] = true; 
+} else {
+ $response['logged_in'] = false;
+}
+
+if($_GET['factor'] === 'ajax') {
+  echo json_encode($response);
+  die();
+}
+
+
+?>
