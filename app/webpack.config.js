69dea47817766c2456b4a003220498a5acb1a64b
Mike Moore <tigerhollowpoint@gmail.com>
2018-04-03 20:43:41 -0400

    MAKING AN APP

---
+++ app/webpack.config.js
@@ -0,0 +1,15 @@
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: "./app.js",//path relative to this file
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js"
+  },
+  plugins: [
+        new HtmlWebpackPlugin({
+            hash: true
+        })
+   ]
+}
