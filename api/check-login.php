8868767b00121b364a2d713a219593c5e36e7385
Mike Moore <tigerhollowpoint@gmail.com>
2018-04-10 14:42:32 -0400

    Update check-login.php

--- api/check-login.php
+++ api/check-login.php
@@ -1,12 +1,19 @@
 <?php
 require '../header.php';
-
+$response = array();
 $logged_in = is_user_logged_in();
-
+$response['logged_in'] = $logged_in;
 if($logged_in) {
  http_response_code(200);
- echo json_encode()
+ $response['user'] = get_user();
+ echo json_encode($response);
+ die();
 }
-  
+//NOT LOGGED IN
+http_response_code(401);
+$_SESSION['login_noonce'] = generate_noonce();
+$response['login_noonce'] = $_SESSION['login_noonce'];
+echo json_encode($response);
+die();
 
 ?>
