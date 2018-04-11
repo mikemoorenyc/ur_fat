e079e8bd3fadfba28d4b35314271feeb3c8a1f12
Mike Moore <tigerhollowpoint@gmail.com>
2018-04-11 14:25:38 -0400

    Update item-form.jsx

--- app/components/item-form.jsx
+++ app/components/item-form.jsx
@@ -1,1 +1,52 @@
+import { h, render, Component  } from 'preact';
+import linkState from 'linkstate';
+import axios from 'axios';
 
+
+export default class ItemForm extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+     opened: false,
+     item: {},
+     method: null
+    }
+    this.resetForm = this.resetForm.bind(this);
+  }
+  
+  componentDidMount() {
+    this.openItemListener = global.emitter.addListener('open-item-form',function(item, method){
+      this.setState({
+        item: item,
+        method: method,
+        opened: true
+      });
+    }.bind(this))
+    
+  }
+  componentWillUnmount() {
+   this.openItemListener.remove();
+  }
+  resetForm() {
+   this.setState({
+    item: {},
+    method: null,
+    opened: false
+   }); 
+  }
+  render(props,state) {
+   if(!state.opened) {
+    return null; 
+   }
+   <div>
+      
+      <button>Cancel</button>   
+   </div>
+    
+    
+  }
+  
+  
+  
+  
+}
