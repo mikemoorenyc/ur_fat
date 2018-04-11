d3078c782346be2611923ef02232601cbcd06ed4
Mike Moore <tigerhollowpoint@gmail.com>
2018-04-11 10:26:56 -0400

    Update add-item-form.js

--- app/components/add-item-form.js
+++ app/components/add-item-form.js
@@ -1,1 +1,20 @@
+import { h, render, Component } from 'preact';
 
+export default class AddItemForm extends Component {
+
+ constructor(props) {
+    super();
+    this.state = {
+      title: '',
+      amount: '',
+      
+    }
+    this.submitForm = this.submitForm.bind(this);
+ } 
+  
+  
+  
+  
+  
+  
+}
