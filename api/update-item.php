594ee3f414d813cc0dfae4619531c357b1e5fec4
Mike Moore <tigerhollowpoint@gmail.com>
2018-04-16 13:37:29 -0400

    Update update-item.php

--- api/update-item.php
+++ api/update-item.php
@@ -4,23 +4,23 @@ require 'header.php';
 
 function badRequest($code=400) {
   http_response_code($code);
-  $_SESSION['update_'.$_POST['id'].'_noonce'] = generate_noonce();
+  $_SESSION['edit_'.$_POST['id'].'_noonce'] = generate_noonce();
   $response = array(
-    'noonce' => $_SESSION['update_'.$_POST['id'].'_noonce'] ,
+    'noonce' => $_SESSION['edit_'.$_POST['id'].'_noonce'] ,
     'local_id' => $_POST['id']
   );
   echo json_encode($response);
   die();
 }
 
-if($_SESSION['update_'.$_POST['id'].'_noonce'] !== $_POST['update_noonce'] || !is_user_logged_in()) {
+if($_SESSION['edit_'.$_POST['id'].'_noonce'] !== $_POST['update_noonce'] || !is_user_logged_in()) {
   badRequest();
 }
-unset($_SESSION['update_'.$_POST['id'].'_noonce']);
+unset($_SESSION['edit_'.$_POST['id'].'_noonce']);
 
 $response = array();
-$_SESSION['update_'.$_POST['id'].'_noonce'] = generate_noonce();
-$response['noonce'] = $_SESSION['update_'.$_POST['id'].'_noonce'];
+$_SESSION['edit_'.$_POST['id'].'_noonce'] = generate_noonce();
+$response['noonce'] = $_SESSION['edit_'.$_POST['id'].'_noonce'];
 
 $updated_item = update_item($_POST['id'],$_POST);
 
