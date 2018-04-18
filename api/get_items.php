6cf3116b076617c81bc82c51543dce246b46ca16
Mike Moore <tigerhollowpoint@gmail.com>
2018-04-18 10:24:44 -0400

    Update get_items.php

--- api/get_items.php
+++ api/get_items.php
@@ -1,1 +1,26 @@
+<?php
+set_include_path('../');
+require 'header.php';
 
+if(!is_user_logged_in()) {
+ http_response_code(401);
+ die();
+}
+
+$offset = $_GET['offset'] ?: "0";
+
+$now = new DateTime();
+$ny_time  = new DateTimeZone('America/New_York');
+$now->setTimezone($ny_time);
+$now->modify(intval($_GET['offset']).' day');
+$startTime =  strtotime($now->format('Y-m-d 00:00:00 O'));
+$endTime = strtotime($now->format('Y-m-d 23:59:59 O'));
+$today_posts = get_posts($startTime, $endTime, get_user()['id']);
+
+$response = array();
+
+$response['offset'] = $offset;
+$response['items'] = $today_posts;
+
+echo json_encode($response);
+die();
