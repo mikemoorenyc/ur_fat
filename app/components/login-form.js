d3b79b7455990372cd08eeb56de0326050262054
Mike Moore <tigerhollowpoint@gmail.com>
2018-04-24 21:07:30 -0400

    finished login

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -3,6 +3,25 @@ import linkState from 'linkstate';
 import axios from 'axios';
 
 
+function LoadingScreen(props) {
+  let statement = "Logging you in";
+  let icon = <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="loader"><line x1="12" y1="2" x2="12" y2="6"></line><line x1="12" y1="18" x2="12" y2="22"></line><line x1="4.93" y1="4.93" x2="7.76" y2="7.76"></line><line x1="16.24" y1="16.24" x2="19.07" y2="19.07"></line><line x1="2" y1="12" x2="6" y2="12"></line><line x1="18" y1="12" x2="22" y2="12"></line><line x1="4.93" y1="19.07" x2="7.76" y2="16.24"></line><line x1="16.24" y1="7.76" x2="19.07" y2="4.93"></line></svg>;
+  if(props.login_verified) {
+    icon = <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="check"><polyline points="20 6 9 17 4 12"></polyline></svg>;
+    statement = "All Set";
+  }
+  return (
+    <div class="login-loader">
+      <div class="content">
+        {icon}
+        <span>{statement}</span>
+      </div>
+
+    </div>
+  )
+}
+
+
 export default class LoginForm extends Component {
   constructor(props) {
     super();
@@ -11,12 +30,23 @@ export default class LoginForm extends Component {
       pass: '',
       error: null,
       checking: false,
-      login_noonce:null
+      login_noonce:null,
+      login_verified: false
     }
     this.submitForm = this.submitForm.bind(this);
+    this.autoFocus = null;
+    this.getFocus = function(element) {
+      this.autoFocus = element;
+    }.bind(this);
+
   }
   componentDidMount() {
+
+    if(this.autoFocus) {
+      this.autoFocus.focus();
+    }
     let formdata = new FormData();
+
     formdata.set('noonce_key',"login_noonce");
     axios({
       method: 'post',
@@ -55,16 +85,22 @@ export default class LoginForm extends Component {
     .then(function (response) {
 
       if(response.data.logged_in) {
-        global.emitter.emit('login-status', true, response.data.user);
+        this.setState({login_verified:true});
+        setTimeout(function(){
+          global.emitter.emit('login-status', true, response.data.user);
+        },2000)
+
       }
       return false;
     }.bind(this))
     .catch(function (error) {
-      console.log(error);
+
       this.setState({
         checking: false,
         error: error.response.data.error_msg,
-        login_noonce: error.response.data.login_noonce
+        login_noonce: error.response.data.login_noonce,
+        email: '',
+        pass: ''
       })
     }.bind(this));
 
@@ -74,16 +110,20 @@ export default class LoginForm extends Component {
 	render(props, state) {
     let loadingScreen = null;
     let disabled = false;
-    if(!state.login_noonce) {
+    if(!state.login_noonce || !state.email || !state.pass) {
       disabled = true;
     }
 
+    let error = null;
+    if(state.error) {
+      error = <div class="error-form">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-alert-circle"><circle stroke-width="1" cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12" y2="16"></line></svg>
+      <span> {state.error}</span>
+    </div>
+    }
+
     if(state.checking) {
-      return (
-        <div style={{backgroundColor: "white",position:"absolute", left: 0, top: 0, right: 0, bottom: 0}} >
-          Loading
-        </div>
-      )
+      return <div id="login-form"><LoadingScreen login_verified={state.login_verified} /></div>
     }
 
 		return (
@@ -91,15 +131,17 @@ export default class LoginForm extends Component {
         <h1>
 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-anchor"><circle cx="12" cy="5" r="3"></circle><line x1="12" y1="22" x2="12" y2="8"></line><path d="M5 12H2a10 10 0 0 0 20 0h-3"></path></svg>Login to ur fat</h1>
 
-        <input placeholder="email address" type="email" required value={state.email}
+        {error}
+
+        <input ref={this.getFocus} placeholder="email address" type="email" required value={state.email}
 						onInput={linkState(this, 'email')} />
 
         <input placeholder="password" type="password" required value={state.pass}
 						onInput={linkState(this, 'pass')} />
 
         <button disabled={disabled} type="submit">Login</button>
-        <div>{state.error}</div>
-    
+
+
       </form>
 
 		);
