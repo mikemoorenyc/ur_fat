6c5993855414c231fd85849a01031de0b9c0ff87
Mike Moore <tigerhollowpoint@gmail.com>
2018-04-26 12:44:33 -0400

    Update listItem.jsx

--- app/components/listItem.jsx
+++ app/components/listItem.jsx
@@ -1,1 +1,39 @@
+import { h, render, Component } from 'preact';
+import DeleteBtn from '../components/DeleteBtn.js';
+import UpdateBtn from '../components/UpdateBtn.js';
 
+export default function ListItem(props) {
+  constructor(props) {
+    super();
+  }
+  
+  
+  render(props,state) {
+   let amt = '';
+   let classList = "list-item ";
+   if(props.openItem === props.item.id) {
+    classList += 'opened'; 
+   }
+   if(props.item.post_amount) {
+    amt =  <span><strong>{props.item.post_amount}</strong> of </span>  
+   }
+   return(
+    <div class={classList}>
+       <h2>{amt}{props.item.post_title}</h2> 
+       <div class="item-controls">
+         <DeleteBtn id={props.item.id}/>
+         <UpdateBtn item={props.item} />
+       </div>
+       
+    </div>
+   
+   ) 
+    
+    
+  }
+  
+  
+  
+  
+  
+}
