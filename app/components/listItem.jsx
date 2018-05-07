58c1d06bf46edd888f584a74215eb8a94e6d2cd1
Mike Moore <tigerhollowpoint@gmail.com>
2018-05-07 15:25:21 -0400

    Update listItem.jsx

--- app/components/listItem.jsx
+++ app/components/listItem.jsx
@@ -7,12 +7,34 @@ var Hammer = require('hammerjs');
 export default class ListItem extends Component {
   constructor(props) {
     super();
-
+    this.state = {
+     del_nonce : null 
+    }
     this.itemDOM = null;
     this.getItemDOM = function(element) {
       this.itemDOM = element;
     }.bind(this);
-
+    this.getDelNonce = this.getDelNonce.bind(this);
+  }
+  getDelNonce() {
+    let formdata = new FormData();
+    let nonce_key =  'edit_'+this.props.item.id+'_noonce' ;
+ 
+    formdata.set('noonce_key',nonce_key);
+    axios({
+      method: 'post',
+      url: window.location.pathname+'api/create-noonce.php',
+      config: { headers: {'Content-Type': 'multipart/form-data' }},
+      data: formdata
+    })
+    .then(function (response) {
+      this.setState({del_nonce: response.data[nonce_key]});
+      
+    }.bind(this))
+    .catch(function (error) {
+      alert('Could not get a login noonce');
+    }) 
+    
   }
   componentDidMount() {
     this.swipeDetector = new Hammer(this.itemDOM);
@@ -20,6 +42,7 @@ export default class ListItem extends Component {
     this.swipeDetector.on('swipe',function(ev){
       if(ev.direction === 2) {
         global.emitter.emit('list-item-open', this.props.item.id);
+        this.getDelNonce();
       }
       if(ev.direction == 4) {
         global.emitter.emit('list-item-open', null);
@@ -31,23 +54,18 @@ export default class ListItem extends Component {
   }
 
   render(props,state) {
-   let amt = '';
-   let of = '';
-   let classList = "list-item ";
+   let amt = (props.item.post_amount)? <span><strong>{props.item.post_amount}</strong> {of} </span> : '',
+       of = (props.item.post_amount && isNaN(props.item.post_amount))? "of " : '',
+       classList = "list-item ";
    if(props.openItem === props.item.id) {
     classList += 'opened';
    }
-   if(props.item.post_amount && isNaN(props.item.post_amount)) {
-     of = 'of '
-   }
-   if(props.item.post_amount) {
-    amt =  <span><strong>{props.item.post_amount}</strong> {of} </span>
-   }
+
    return(
     <div ref={this.getItemDOM} class={classList}>
        <div class="slider">
         <div class="post-title">{amt}{props.item.post_title}</div>
-        <DeleteBtn id={props.item.id}/>
+        <DeleteBtn del_nonce={state.del_nonce} id={props.item.id}/>
         <UpdateBtn item={props.item} />
        </div>
     </div>
