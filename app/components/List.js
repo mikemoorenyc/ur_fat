01bb8d816feac65fb36279fb94e685616b72a6a7
Mike Moore <tigerhollowpoint@gmail.com>
2018-05-16 15:31:26 -0400

    Update List.js

--- app/components/List.js
+++ app/components/List.js
@@ -7,7 +7,8 @@ export default function List(props) {
   if(!props.today_posts.length) {
       return false;
     }
-
+    let loadingClass = (props.fetching_posts && !props_today_posts.length)? "loading" : '';
+    let emptyState = (!props.fetching_posts && !props_today_posts.length) <div className="empty">You haven&rsquo;t eaten anything today.</div> : false;
     let items = props.today_posts.map(function(e,i){
 
       return (
@@ -15,7 +16,8 @@ export default function List(props) {
       );
     }.bind(this));
     return (
-      <div className="list-items">
+      <div className={"list-items "+loadingClass}>
+      {emptyState}
       {items}
       </div>
     )
